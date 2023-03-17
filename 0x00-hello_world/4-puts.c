67fd8d75c4a69d776b1a6da8bd33ef4f4cf5b302
BBoltzmann <woleoshoifeoluwa@gmail.com>
2023-03-17 01:00:54 +0100

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - puts uning puts to print "Programming is like building a multilingual puzzle
+ * main - puts uning puts to print "Programming is like building
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
