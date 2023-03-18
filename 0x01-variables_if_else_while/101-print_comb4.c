959311ae4480b57159e3285f96e9bc46184d65ae
bboltzmann <woleoshoifeoluwa@gmail.com>
2023-03-17 20:49:47 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,20 +5,23 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int main(void) {
 
-	{
-	int a;
-
-	for (a = 0; a < 10; a++)
-	{
-	putchar (a + '0');
-	if (a != 9)
-	{
-	putchar (',');
-	putchar (' ');
-	}
-	}
-	putchar ('\n');
-	return (0);
-	}
+int i; 
+int j;
+int k;
+	for (i = 0; i <= 7; i++) {
+        for (j = i + 1; j <= 8; j++) {
+            for (k = j + 1; k <= 9; k++) {
+                putchar(i + '0');
+                putchar(j + '0');
+                putchar(k + '0');
+                if (i < 7) {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    return 0;
+}
