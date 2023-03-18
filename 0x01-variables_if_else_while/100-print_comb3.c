d3e40dfed7def340ec8436269e6d46274b172e45
bboltzmann <woleoshoifeoluwa@gmail.com>
2023-03-17 22:00:46 -0700

    help

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,22 +7,22 @@
 
 int main(void)
 
-	{
-	int a,b;
+        {
+        int a,b;
 
-	for (a = 0; a <= 8; a++)
-	{
-	for (a = b + 1; b <= 9; b++)
-	{
-	putchar (a + '0');
-	putchar (b + '0');
-	if (a < 8 || b < 9)
-	{
-	putchar (',');
-	putchar (' ');
-	}
-	}
-	}
-	putchar ('\n');
-	return (0);
-	}
+        for (a = 0; a <= 8; a++)
+        {
+        for (a = b + 1; b <= 9; b++)
+        {
+        putchar (a + '0');
+        putchar (b + '0');
+        if (a < 8 || b < 9)
+        {
+        putchar (',');
+        putchar (' ');
+        }
+        }
+        }
+        putchar ('\n');
+        return (0);
+        }
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,21 +4,32 @@
  *
  * Return: Always 0 (Success)
  */
+int main() {
+    for (int i = 0; i <= 99; i++) {
+        for (int j = i; j <= 99; j++) {
+            int tens_i = i / 10;
+            int ones_i = i % 10;
+            int tens_j = j / 10;
+            int ones_j = j % 10;
+            
+            // check if the two numbers are the same
+            if (i == j) {
+                continue;
+            }
+            
+            // print the numbers in ascending order
+            if (tens_i < tens_j || (tens_i == tens_j && ones_i < ones_j)) {
+                putchar(tens_i + '0');
+                putchar(ones_i + '0');
+                putchar(' ');
+                putchar(tens_j + '0');
+                putchar(ones_j + '0');
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    
+    return 0;
+}
 
-int main(void)
-
-	{
-	int a;
-
-	for (a = 0; a < 10; a++)
-	{
-	putchar (a + '0');
-	if (a != 9)
-	{
-	putchar (',');
-	putchar (' ');
-	}
-	}
-	putchar ('\n');
-	return (0);
-	}
