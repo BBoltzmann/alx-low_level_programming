c9bddb1d86471e8bb6249f96ed01262f20c54e55
bboltzmann <woleoshoifeoluwa@gmail.com>
2023-03-21 22:03:12 -0700

    init

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,10 @@
 void print_alphabet(void)
 {
 char ch;
-
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-        _putchar (ch);
-        }
-        _putchar ('\n');
-        return;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar (ch);
+}
+_putchar ('\n');
+return;
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * main - Program that prints _putchar
+ * main - Program that prints alphabeth
  *
  * Return: Always 0 (Success)
  */
-
 void print_alphabet_x10(void)
 {
 char ch;
