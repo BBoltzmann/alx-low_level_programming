39e528e37eb177c7315a1b9cec8f72765a3f1904
bboltzmann <woleoshoifeoluwa@gmail.com>
2023-03-23 11:22:55 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_most_numbers - Program that identifies int 0-9
+ * Return: x the miltiple of and b
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (y = 0; y <= 10; y++)
+	{
+	for (x = 0; x <= 14; x++)
+	{
+	if (x > 9)
+	{
+	_putchar ((x / 10) + '0');
+	}
+	_putchar ((x % 10) + '0');
+	}
+	_putchar (10);
+	}
+	
+}
