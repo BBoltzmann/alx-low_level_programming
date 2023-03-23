cf2e89368e397827173d4a07cd974166042a2839
bboltzmann <woleoshoifeoluwa@gmail.com>
2023-03-23 09:27:59 -0700

    funtion for multiplcation of 2 int

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Program that identifies int 0-9
+ * @a: integer
+ * @b: integer
+ * Return: x the miltiple of and b
+ */
+
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+	return (x);
+}
