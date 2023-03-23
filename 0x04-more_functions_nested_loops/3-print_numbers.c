ea3ec4c182583f27250e9ea780b547af3a2f5b3f
bboltzmann <woleoshoifeoluwa@gmail.com>
2023-03-23 10:19:31 -0700

    print numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Program that identifies int 0-9
+ * Return: x the miltiple of and b
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+	_putchar (x);
+	}
+	_putchar (10);
+}
