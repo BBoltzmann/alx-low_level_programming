aa2ec09fd4544eec89921fa7eb97e8fb4a258340
bboltzmann <woleoshoifeoluwa@gmail.com>
2023-03-23 18:31:32 -0700

    print line with underscore

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - Program that prints lines
+ * @n: integer for line lenght
+ * Return: x the miltiple of and b
+ */
+
+void print_line(int n)
+{
+	int y;
+
+	if (n >= 1)
+	{
+	for (y = 0; y <= n; y++)
+	{
+	_putchar (95);
+	}
+	_putchar (10);
+	}
+	else
+	{
+	_putchar (10);
+	}
+}
