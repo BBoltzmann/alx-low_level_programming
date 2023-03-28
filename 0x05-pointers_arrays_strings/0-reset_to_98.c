daec015fa1fb5f59074dc528ca871d9e0f33ef06
bboltzmann <woleoshoifeoluwa@gmail.com>
2023-03-27 19:42:20 -0700

    initializing folder

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - Program that identifies int 0-9
+ * @c: intger
+ * Return: 1 if c is in int 0-9 and 0 otherwise
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
