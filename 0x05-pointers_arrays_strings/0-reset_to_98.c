f447a2daa9eb5c8af6f74a4cca482b2d67081222
bboltzmann <woleoshoifeoluwa@gmail.com>
2023-03-27 20:02:58 -0700

    swap and corrections on reset_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _isdigit - Program that identifies int 0-9
- * @c: intger
+ * reset_to_98 - Program that resetsthe value of int n to 98
+ * @n: intger with pointer
  * Return: 1 if c is in int 0-9 and 0 otherwise
  */
 
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - Program that swaps the values of n = 2 int 
+ * @a: intger with pointer
+ * @b: intger with pointer
+ * Return: 1 if c is in int 0-9 and 0 otherwise
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
