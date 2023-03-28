1a7f931a52e23f7879d85080ff09312dad21b83e
bboltzmann <woleoshoifeoluwa@gmail.com>
2023-03-28 13:16:23 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_half - Program that prints a string to stdout
+ * @str: char with pointer
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int x;
+	int y = 0;
+	int n;
+
+	while (str[y] != '\0')
+	{
+	y++;
+	}
+
+	n = (y - 1) / 2;
+	for (x = n + 1; x <= y; x++)
+	_putchar(str[x]);
+	_putchar('\n');
+}
