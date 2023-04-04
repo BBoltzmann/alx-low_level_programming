7c222533b9ffda17e846770310c41cd757525d7c
bboltzmann <woleoshoifeoluwa@gmail.com>
2023-04-03 21:18:12 -0700

    find a char loop through string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - Program that locates a character in a string
+ * @s: char pointer
+ * @c: char to be located
+ * Return: dest
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	if (c == s[i])
+	return (s + i);
+	}
+	return (NULL);
+}
