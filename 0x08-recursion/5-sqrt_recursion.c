b7b5b69f1ac17138bf6b9b2001c7093e5245d458
bboltzmann <woleoshoifeoluwa@gmail.com>
2023-04-13 02:29:26 -0700

    find natural root without for loop

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,38 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * find_sqrt - helper function to recursively find square root
+ * @n: integer to find the square root of
+ * @left: left bound of search interval
+ * @right: right bound of search interval
+ * Return: integer square root of n within search interval, or -1 if not found
+ */
+int find_sqrt(int n, int left, int right)
+{
+	int mid = (left + right) / 2;
+	int mid_sq = mid * mid;
+
+	if (left > right)
+		return (-1);
+	else if (mid_sq == n)
+		return (mid);
+	else if (mid_sq < n)
+		return (find_sqrt(n, mid + 1, right));
+	else
+		return (find_sqrt(n, left, mid - 1));
+}
+
+/**
  * _sqrt_recursion - Program that returns the natural square root of a number
  * @n: int
  * Return: int
  */
+
 int _sqrt_recursion(int n)
 {
-	int i = 1;
-
 	if (n < 0)
 	{
 		return (-1);
 	}
-	if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	for (; i < n / 2; i++)
-	{
-		if (i * i == n)
-		{
-			return (i);
-		}
-		if (i * i > n)
-		{
-			break;
-		}
-	}
-	return (-1);
+	return (find_sqrt(n, 0, n));
 }
