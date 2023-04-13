1ac6ceb6d7872f4879b516aeb77d3613c13bf7de
bboltzmann <woleoshoifeoluwa@gmail.com>
2023-04-13 03:54:35 -0700

    prime calculator without loop

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,32 +1,34 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * test_prime - Program that returns prime number, otherwise return 0
+ * @n: int
+ * @i: int equal 1
+ * Return: int
+ */
+int test_prime(int n, int i)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+	if (n % i == 0 && i > 1)
+	{
+		return (0);
+	}
+	if ((n / i) < i)
+	{
+		return (1);
+	}
+	return (test_prime(n, i + 1));
+}
+
+/**
  * is_prime_number - Program that returns prime number, otherwise return 0
  * @n: int
  * Return: int
  */
 int is_prime_number(int n)
 {
-	int i = 3;
-
-	if (n < 2)
-	{
-		return (0);
-	}
-	if (n == 2 || n == 3)
-	{
-		return (1);
-	}
-	if (n % 2 == 0)
-	{
-		return (0);
-	}
-	for (; i * i <= n; i += 2)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (test_prime(n, 1));
 }
